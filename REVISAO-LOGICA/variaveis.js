ba05a15135ab80aa2bdf0b99f4a6ec28bec9c7de
Alexandre <lamontagna@gmail.com>
2021-02-25 10:51:05 -0300

    curso Js 25/02/2021

---
+++ REVISAO-LOGICA/variaveis.js
@@ -0,0 +1,19 @@
+// var, let, const
+
+// var -> ES5 - cross-browser
+// let e const -> ES2015
+
+let teste = "minha string";
+teste = 10;
+
+let teste2 = 10;
+
+let teste3;
+
+console.log(teste2);
+
+teste3 = 20;
+console.log(teste3);
+
+const teste4 = "eu sou imutável";
+console.log(teste4);
